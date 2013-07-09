1ddf948e770c998afbee5ab98dfd34d4f082d43c
misterbeebee <github_public@mrbb.org>
2013-07-09 08:46:22 -0700

    At interval, Auto-reload based on app/ md5 change, instead of always.

--- build.sh
+++ build.sh
@@ -4,7 +4,11 @@ rm -rf ~/Sites/calisson/app
 mkdir -p ~/Sites/calisson/app
 
 while true; do
-  rsync -ra app/ ~/Sites/calisson/app/
+  site=~/Sites/calisson/
+  rsync -ra app/ $site/app/
+  # Write a signature, so users can detect changes
+  tar c app | md5 > $site/app/md5sum
+  # cat $site/app/md5sum
   # open "http://localhost/~$USER/calisson/app/"
   sleep 3
 done
